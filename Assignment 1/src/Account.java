27256fd5afd63426bd373a98201ba356e899775e
Siddharth-Sangwan <91517742+Siddharth-Sangwan@users.noreply.github.com>
2021-09-29 12:57:46 +0530

    Add files via upload

--- Assignment 1/src/Account.java
+++ Assignment 1/src/Account.java
@@ -1,14 +1,14 @@
 
 public class Account {
     private String owner_name;  
-    private boolean acc_type;  
+    private int acc_type;  
     private float balance;  
-    private String acc_status;
+    private String acc_status = "Valid";
     //private Local.date date_created;
     private int pin_code;
     private String city, state; 
     
-    public Account(String owner_name, boolean acc_type)
+    public Account(String owner_name, int acc_type)
     {
     	this.owner_name = owner_name;
     	this.acc_type = acc_type;
@@ -23,11 +23,45 @@ public class Account {
     
     public void deposit(float deposit_amt)
     {
-    	
+    	this.balance = this.balance + deposit_amt;
+    	System.out.println("----------------------------------------");
+    	System.out.println(" Balance: " + this.balance);
+    	System.out.println("----------------------------------------");
     }
     
     public void withdraw(float withdraw_amt)
     {
-    	
+    	if(withdraw_amt <= balance)
+    	{    	
+    		System.out.println("----------------------------------------");
+    		this.balance = this.balance - withdraw_amt;
+    		System.out.println(" Balance: " + this.balance);
+    		System.out.println("----------------------------------------");
+    	}
+    	else
+    	{
+    		System.out.println("----------------------------------------");
+    		System.out.println(" Insufficient Balance");   		
+    		System.out.println("----------------------------------------");
+    	}
+    }
+    
+    public void displayAll()
+    {
+    	System.out.println("----------------------------------------");
+    	System.out.println(" Owner Name: " + this.owner_name +"\n Balance: " + this.balance + "\n Account Status: " + this.acc_status);
+    	if(acc_type == 0)
+    		System.out.println(" Account Type: Savings");
+    	else
+    		System.out.println(" Account Type: Current");
+    	System.out.println(" Your Address: \n City: " + this.city +"\n State: "+ this.state +"\n Pin Code:" + this.pin_code);
+    	System.out.println("----------------------------------------");
+    }
+    
+    public void balance()
+    {
+    	System.out.println("----------------------------------------");
+    	System.out.println(" Balance: " + this.balance);
+    	System.out.println("----------------------------------------");
     }
 }
--- Assignment 1/src/Bank.java
+++ Assignment 1/src/Bank.java
@@ -9,7 +9,7 @@ public class Bank {
 		System.out.println("Account Creation");
 		String name,city, state;
 		int pin_code;
-		boolean acc_type;
+		int acc_type;
 		float deposit_amt;
 		float withdraw_amt;
 		Scanner scn = new Scanner(System.in);
@@ -25,8 +25,8 @@ public class Bank {
 		System.out.println("Enter your Pin Code:");
 		pin_code = scn.nextInt();
 		
-		System.out.println("Enter your Account Type:(0---->Savings    1---->Current");
-		acc_type = scn.nextBoolean();
+		System.out.println("Enter your Account Type:(0---->Savings    1---->Current)");
+		acc_type = scn.nextInt();
 		
 		Account userAccount = new Account(name,acc_type);
 		userAccount.setAddress(city, state, pin_code);
@@ -34,37 +34,41 @@ public class Bank {
 		while(choice !=5) {
 			
 			System.out.println("Select an option to continue:");
-			System.out.println("1. Deposit \n 2. Withdraw \n 3. View Details \n 4. View Balance \n 5. Exit");
+			System.out.println(" 1. Deposit \n 2. Withdraw \n 3. View Details \n 4. View Balance \n 5. Exit");
 			choice = scn.nextInt();
 			
 			switch(choice)
 			{
 				case 1:
-					System.out.println("Enter amount to be deposited: ");
+					System.out.println(" Enter amount to be deposited: ");
 					deposit_amt = scn.nextInt();
 					userAccount.deposit(deposit_amt);
+					break;
 					
 				case 2:
-					System.out.println("Enter amount to be withdrawn: ");
-					deposit_amt = scn.nextInt();
+					System.out.println(" Enter amount to be withdrawn: ");
+					withdraw_amt = scn.nextInt();
 					userAccount.withdraw(withdraw_amt);
+					break;
 				
 				case 3:
+					userAccount.displayAll();
+					break;
 				
 				case 4:
+					userAccount.balance();
+					break;
 					
 				case 5:
+					break;
 					
 				default:
-				
-			
+					System.out.println(" Enter a valid choice!!!!");
+					break;
+						
 			}
 		}
-		
-		
-		
-		
-		 
+			 
 	}
-
+	
 }
